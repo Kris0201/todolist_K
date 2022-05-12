310c8d19b9210f0ee7275816db8b053f6fb2c326
Kris Tung <ragdoll.washoku89@gmail.com>
2022-05-12 14:43:09 +0800

    整理註記

--- todolist.js
+++ todolist.js
@@ -1,18 +1,15 @@
 //先建立新增畫面
 
-//抓到輸入在待辦事項input中的值
+
 const txt=document.querySelector('.txt')
-
 const list=document.querySelector('.list')
-
-//點擊 .btn_add 時取得 txt.value
 const btnAdd=document.querySelector('.btn_add')
 
 let todoData=[]
 //在dev tool頁面直接搜todoData，確認輸入的input資料有進到陣列中
 
 btnAdd.addEventListener('click', (e)=>{
-    //判斷輸入不能為空白
+    //判斷輸入不能為空白&清除空格
     if(txt.value.trim()==""){
         return;
     }
@@ -39,16 +36,16 @@ btnAdd.addEventListener('click', (e)=>{
     </li>
     */
 
-    let str="";  //不能是const，要是let
-    str+=`<li>
-    <label class="checkbox" for="">
-    <input type="checkbox" />
-    <span>${txt.value}</span>
-    </label>
-    <a href="#" class="delete"></a>
-    </li>`
+    // let str="";  //不能是const，要是let
+    // str+=`<li>
+    // <label class="checkbox" for="">
+    // <input type="checkbox" />
+    // <span>${txt.value}</span>
+    // </label>
+    // <a href="#" class="delete"></a>
+    // </li>`
 
-    list.innerHTML=str
+    // list.innerHTML=str
 })
 
 
